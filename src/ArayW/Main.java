ce1f3bde2c3fa343ec6034c12d25381166fa48dc
nikas <vadikin@rambler.ru>
2016-10-06 20:30:15 +0300

    array sort

--- src/ArayW/Main.java
+++ src/ArayW/Main.java
@@ -2,6 +2,7 @@ package ArayW;
 
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -22,5 +23,28 @@ public class Main {
         {
             System.out.print(value + "  ");
         }
+        Arrays.sort(first_array);
+        System.out.println();
+        for (int value: first_array)
+        {
+            System.out.print(value + "  ");
+        }
+        String[] s_array = new String[4];
+        s_array[0] = "vasya";
+        s_array[1] = "peta";
+        s_array[2] = "sveta";
+        s_array[3] = "ola";
+        System.out.println();
+        for (String value: s_array)
+        {
+            System.out.print(value + "!");
+        }
+        Arrays.sort(s_array);
+        System.out.println();
+        for (String value: s_array)
+        {
+            System.out.print(value + "!");
+        }
+
     }
 }
