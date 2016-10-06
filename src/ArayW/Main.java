555cf99433c8b9fbd2add2f6c3bcbb07cda2e4b2
nikas <vadikin@rambler.ru>
2016-10-06 20:13:53 +0300

    randome

--- src/ArayW/Main.java
+++ src/ArayW/Main.java
@@ -1,5 +1,9 @@
 package ArayW;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Random;
+
 /**
  * Created by User on 06.10.2016.
  */
@@ -8,9 +12,15 @@ public class Main {
     {
 
         int[] first_array = new int[10];
+        Random rnd = new Random();
+
         for (int i= 0; i< first_array.length; i++)
         {
-            first_array[i]= i+1;
+            first_array[i]= rnd.nextInt(100);
+        }
+        for (int value: first_array)
+        {
+            System.out.print(value + "  ");
         }
     }
 }
